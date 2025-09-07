c8267ba3b445e1bc517342b94072189927af9be8
xwang_ <gary.xi.wang@outlook.com>
2025-09-07 19:49:24 +0800

    fix: 修复OrderType枚举str

--- server/app/models/__init__.py
+++ server/app/models/__init__.py
@@ -3,7 +3,7 @@ from app.core.database import Base
 # 导入所有模型以确保它们被注册到Base.metadata中
 from .user import User, UserRole
 from .category import Category, CategoryType
-from .order import Order, OrderStatus, OrderType, DesignCycle
+from .order import Order, OrderStatus, DesignCycle
 from .progress import Progress
 from .split import Split, QuoteStatus
 # from .design import Design
