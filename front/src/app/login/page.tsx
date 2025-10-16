3161e6f1ff90ba467b2480eb20eecc46d558b2f8
xwang_ <gary.xi.wang@outlook.com>
2025-10-16 09:52:58 +0800

    fix: 修复前端缓存问题

--- front/src/app/login/page.tsx
+++ front/src/app/login/page.tsx
@@ -1,8 +1,5 @@
 "use client";
 
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
-
 import React, { useState } from "react";
 import { Form, Input, Button, Typography, message } from "antd";
 import { UserOutlined, LockOutlined } from "@ant-design/icons";
